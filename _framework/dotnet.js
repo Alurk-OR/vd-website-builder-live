f84c763a07c4aca8866b421f3c1980f1f448af29
Alurk-OR <abasevanton447@yandex.ru>
2026-03-29 17:13:30 +0300

    Fix: update dotnet.js shim to new hash

--- _framework/dotnet.js
+++ _framework/dotnet.js
@@ -1,2 +1,2 @@
-export * from './dotnet.3hrd0gueq3.js';
-export { default } from './dotnet.3hrd0gueq3.js';
+export * from './dotnet.drb62zqnp5.js';
+export { default } from './dotnet.drb62zqnp5.js';
